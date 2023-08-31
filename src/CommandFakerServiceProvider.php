93af8400d3f55d0d442eef3f70f8e718fc44285f
Deborah White <deborah.white1503@icloud.com>
2023-08-31 19:31:16 +0200

    Simplify faking commands

--- src/CommandFakerServiceProvider.php
+++ src/CommandFakerServiceProvider.php
@@ -3,50 +3,59 @@
 namespace Pj\CommandFaker;
 
 use Illuminate\Console\Events\ArtisanStarting;
-use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Pj\CommandFaker\Support\FakeCommandRepository;
-use Symfony\Component\Console\ConsoleEvents;
-use Symfony\Component\Console\Event\ConsoleErrorEvent;
-use Symfony\Component\Console\Exception\CommandNotFoundException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class CommandFakerServiceProvider extends ServiceProvider
 {
-    /** @noinspection PhpUnusedParameterInspection */
     public function register(): void
     {
         $this->app->singleton(FakeCommandRepository::class);
 
-        if (! $this->app->runningInConsole() || $this->app->runningUnitTests()) {
+        if (! $this->app->runningInConsole()) {
             return;
         }
 
-        Event::listen(ArtisanStarting::class, function (ArtisanStarting $_) {
-            try {
-                $kernel = resolve(Kernel::class);
-                $reflection = new \ReflectionClass(get_class($kernel));
-                /** @var null|EventDispatcherInterface $symfonyDispatcher */
-                $symfonyDispatcher = $reflection->getProperty('symfonyDispatcher')->getValue($kernel);
-                $symfonyDispatcher?->addListener(ConsoleEvents::ERROR, function (ConsoleErrorEvent $event) {
-                    if (! ($event->getError() instanceof CommandNotFoundException)) {
-                        return;
-                    }
-                    $command = $event->getInput()->getFirstArgument();
-                    if (commandFaker()->matches($command)) {
-                        $event->getOutput()->writeln('This command is not implemented, but was faked by "CommandFaker"', OutputInterface::VERBOSITY_VERBOSE);
-                        $event->setExitCode(0);
-                    }
-                });
-            } catch (\Throwable $exception) {
-                if (commandFakerShouldIgnoreErrors()) {
+        $this->booted(function () {
+            Event::listen(ArtisanStarting::class, function (ArtisanStarting $event) {
+                if (! ($commandFaker = commandFaker())->hasFakedCommands()
+                    || blank(($commandName = (new ArgvInput())->getFirstArgument()))
+                ) {
                     return;
                 }
 
-                throw $exception;
-            }
+                if (! ($commandFaker->matches($commandName))) {
+                    return;
+                }
+
+                $event->artisan->add(new class($commandName) extends Command
+                {
+                    public function __construct(string $name = null)
+                    {
+                        parent::__construct($name);
+                        $this->ignoreValidationErrors();
+                        $this->setDescription('This command is not implemented, but was faked by "CommandFaker"');
+                    }
+
+                    protected function execute(InputInterface $input, OutputInterface $output): int
+                    {
+                        $output->writeln(
+                            $this->getDescription(),
+                            OutputInterface::VERBOSITY_VERBOSE
+                        );
+
+                        return 0;
+                    }
+                });
+
+            });
+
         });
+
     }
 }
